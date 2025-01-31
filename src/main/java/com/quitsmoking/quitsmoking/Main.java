d1056c2e646f0b289b49658929bf97b0325e8ced
Pranay <pranaygodghate13@gmail.com>
2025-01-31 21:18:10 +0530

    Class Person made, now starting Spring JDBC implementation

---
+++ src/main/java/com/quitsmoking/quitsmoking/Main.java
@@ -0,0 +1,11 @@
+package com.quitsmoking.quitsmoking;
+
+import com.quitsmoking.quitsmoking.model.Person;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Person satish=new Person();
+    }
+    
+}
---
+++ src/main/java/com/quitsmoking/quitsmoking/model/Person.java
@@ -0,0 +1,49 @@
+package com.quitsmoking.quitsmoking.model;
+
+public class Person {
+    private String name;
+    private int age;
+    private String gender;
+    private int years;
+    private int months;
+    private int number;
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public String getGender() {
+        return gender;
+    }
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+    public int getYears() {
+        return years;
+    }
+    public void setYears(int years) {
+        this.years = years;
+    }
+    public int getMonths() {
+        return months;
+    }
+    public void setMonths(int months) {
+        this.months = months;
+    }
+    public int getNumber() {
+        return number;
+    }
+    public void setNumber(int number) {
+        this.number = number;
+    }
+    
+
+    
+}
